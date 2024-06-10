18fba5c34343ff4ec2e9502cfefb0e2f921451a3
SrGalafoice <133278413+SrGalafoice@users.noreply.github.com>
2024-06-09 21:27:50 -0300

    Create InicioFragment.java

---
+++ src/Biblioteca/java/InicioFragment.java
@@ -0,0 +1,23 @@
+package br.edu.fateczl.biblioteca;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InicioFragment extends Fragment {
+    private View view;
+    public InicioFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_inicio, container, false);
+    return view;
+    }
+}
