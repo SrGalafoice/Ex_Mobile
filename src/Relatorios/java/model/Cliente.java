154e553a17d8beb57a997f36b1435d3f95501153
SrGalafoice <133278413+SrGalafoice@users.noreply.github.com>
2024-06-28 00:09:09 -0300

    Create Cliente.java

---
+++ src/Relatorios/java/model/Cliente.java
@@ -0,0 +1,24 @@
+package br.edu.fateczl.relatorios.model;
+
+public class Cliente extends Pessoa {
+    private String cpf, endereco;
+
+    public Cliente() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
